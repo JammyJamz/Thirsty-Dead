89dc39d2f8bd2bba525309b21494dc901ed25c05
JammyJamz <33839743+JammyJamz@users.noreply.github.com>
2019-09-28 19:38:15 -0400

    staminaupdate

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public Transform dest;
+
+    private void OnMouseDown()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().freezeRotation = true;
+        this.transform.position = dest.transform.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
+    }
+
+    private void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().freezeRotation = false;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<BoxCollider>().enabled = true;
+    }
+
+
+
+
+
+
+
+}
