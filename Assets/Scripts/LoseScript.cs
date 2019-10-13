6794842bfb0e0185d96935a7ed9657bcf6dfe16f
Ivana-Pagan <37190657+Ivana-Pagan@users.noreply.github.com>
2019-10-13 19:33:15 -0400

    Did the lose scene

---
+++ Assets/Scripts/LoseScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseScript : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Load Quit"); //Confirming this is quiting while in Editor.
+        Application.Quit();
+    }
+}
